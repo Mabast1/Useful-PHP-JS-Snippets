1786d4f57ec6150a60594065760fe1af5868b669
Mabast <mabast.niga@yahoo.com>
2022-04-13 00:51:12 -0500

    creating cookies to hide elements 🍪

---
+++ creating-cookies-to-hide-element.php
@@ -0,0 +1,13 @@
+add_action('init', function() {
+    if (!isset($_COOKIE['training_banner_cookie'])) {
+        setcookie('training_banner_cookie', 'showbanner', strtotime('+1 day'));
+    }
+	$cookieValue = $_COOKIE['training_banner_cookie'];
+	if (//condition to find a class in the dome)){
+		?><style>#training-alert{display:none;}</style> <?php
+		setcookie('training_banner_cookie', 'hidebanner', strtotime('+1 day'));
+	}
+	if ($cookieValue == "hidebanner"){
+		?><style>#training-alert{display:none;}</style> <?php
+	}
+});
