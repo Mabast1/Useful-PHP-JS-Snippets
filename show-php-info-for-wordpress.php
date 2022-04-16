b9ede0df7baed8fd08b7179071ec5e863decdb70
Mabast <mabast.niga@yahoo.com>
2022-04-16 04:19:18 -0500

    Create show-php-info-for-wordpress.php

---
+++ show-php-info-for-wordpress.php
@@ -0,0 +1,9 @@
+<?php
+/*Create info.php in your application webroot directory with this function.
+This is to display the php info and config
+*/
+
+// Show all information, defaults to INFO_ALL
+phpinfo();
+
+?>
